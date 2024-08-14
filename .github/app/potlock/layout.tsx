bf61beb340b930e27ec80e818120fb45c77716d6
kurodenjiro <kurodenjiro1@gmail.com>
2024-08-15 00:44:49 +0700

    add new source

--- .github/app/potlock/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { EndpointsContext } from "./agent";
-import { ReactNode } from "react";
-import { WalletSelectorContextProvider } from "@/app/contexts/WalletSelectorContext"
-
-export default function RootLayout(props: { children: ReactNode }) {
-  return <EndpointsContext>{props.children}</EndpointsContext>;
-}
