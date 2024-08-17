7f54cd2b1a04387b95564cddd56bbcef05535de3
kurodenjiro <kurodenjiro1@gmail.com>
2024-08-18 05:43:20 +0700

    fix eslint

--- app/potlock/agent.tsx
+++ app/potlock/agent.tsx
@@ -3,7 +3,11 @@ import "server-only";
 import { agentExecutor } from "./ai/chain";
 import { exposeEndpoints, streamRunnableUI } from "./utils/server";
 import { ChatMessage } from "@langchain/core/messages";
-
+import {
+  Runnable,
+  RunnableConfig,
+  RunnableLambda,
+} from "@langchain/core/runnables";
 
 async function agent(inputs: {
   input: string;
@@ -11,7 +15,7 @@ async function agent(inputs: {
 }) {
   "use server";
   
-  return streamRunnableUI(agentExecutor, {
+  return streamRunnableUI(agentExecutor  as unknown as Runnable, {
     input: inputs.input,
     chat_history: inputs.chat_history.map(
       ([role, content]) => new ChatMessage(content, role),
