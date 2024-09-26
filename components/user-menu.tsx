b568619bd65f49ec2cf930da1562e6fb6ecbd422
kurodenjiro <kurodenjiro1@gmail.com>
2024-09-26 15:22:55 +0700

    fix wallet connect

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import { useWalletSelector } from "@/app/contexts/WalletSelectorContext"
 
 import { type Session } from '@/lib/types'
